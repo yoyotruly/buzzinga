7e94537665918138b4e2c3b920aa192f2b7e518f
Yoyo Yang <yoyo.truly@gmail.com>
2022-09-27 21:37:04 -0400

    Add home page structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <>
+      <main>
+        This is the main section
+        <h1>Tag line</h1>
+        <p>Some text</p>
+        <button>Go Explore</button>
+      </main>
+      
+      <aside>
+        This is the side section
+        <div>Product card 1</div>
+        <button>Next</button>
+        <div>Product card 2</div>
+      </aside>
+    </>
   );
 }
