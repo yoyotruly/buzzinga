76dd5a6238c323ed85b9453a7d9b9463c30647f4
Yoyo Yang <yoyo.truly@gmail.com>
2022-09-27 21:59:51 -0400

    Add product detail page structure

--- pages/products/[pid].tsx
+++ pages/products/[pid].tsx
@@ -1,12 +1,34 @@
-import { useRouter } from "next/router"
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Product({ product }) {
-  const router = useRouter()
-  const { pid } = router.query
+  const router = useRouter();
+  const { pid } = router.query;
 
   return (
-    <div>
-      <h1>Product { pid } detail page</h1>
-    </div>
-  )
+    <main>
+      <section>
+        <h1>Product {pid} Name</h1>
+        <div>Description and stats</div>
+        <div>Product image</div>
+        <div>Product side stats</div>
+      </section>
+
+      <section>
+        <h2>You may also like</h2>
+        <div>
+          <div>
+            <Link href="/products/[pid]" as={"/products/1"}>
+              Product card 1
+            </Link>
+          </div>
+          <div>
+            <Link href="/products/[pid]" as={"/products/2"}>
+              Product card 2
+            </Link>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
