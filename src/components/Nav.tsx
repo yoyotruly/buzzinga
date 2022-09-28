6a8919467969bbbdc89875df1e1ab7df516496c4
Yoyo Yang <yoyo.truly@gmail.com>
2022-09-27 20:22:08 -0400

    Add Nav component skeleton

---
+++ src/components/Nav.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li><Link href="/">Buzzinga</Link></li>
+        <li><Link href="/products">Products</Link></li>
+        <li><Link href="/about">About Me</Link></li>
+        <li><Link href="/contacts">Contacts</Link></li>
+      </ul>
+    </nav>
+  );
+}
