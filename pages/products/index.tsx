cfc576c489c56a5647d14c7136df0ee5ccdcec4b
Yoyo Yang <yoyo.truly@gmail.com>
2022-09-27 21:54:06 -0400

    Add products page structure

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,11 +1,40 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Products() {
   return (
-    <div>
-      <h1>Products page</h1>
+    <>
+      <nav>
+        <section>
+          <h2>Category</h2>
+          <div>Pills</div>
+        </section>
+        <section>
+          <h2>Sub-Category</h2>
+          <div>Pills</div>
+        </section>
+        <section>
+          <h2>Price Range</h2>
+          <div>Slider</div>
+        </section>
+        <section>
+          <h2>Sugar Content</h2>
+          <div>Slider</div>
+        </section>
+      </nav>
 
-      <Link href="/products/[pid]" as={"/products/3"}>Product detail</Link>
-    </div>
-  )
+      <main>
+        <h1>Products List</h1>
+        <div>
+          <Link href="/products/[pid]" as={"/products/1"}>
+            Product card 1
+          </Link>
+        </div>
+        <div>
+          <Link href="/products/[pid]" as={"/products/2"}>
+            Product card 2
+          </Link>
+        </div>
+      </main>
+    </>
+  );
 }
