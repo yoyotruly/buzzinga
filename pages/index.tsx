82a8409f1ed49eae8a170f44a131f07d7d26f9d3
Yoyo Yang <yoyo.truly@gmail.com>
2022-09-27 20:23:22 -0400

    Refactor page head and other layout away from index file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,7 @@
-import Head from "next/head";
-import Link from "next/link";
-
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Buzzinga</title>
-        <meta name="description" content="Find what to drink next" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <nav>
-        <Link href="/products">Products</Link>
-      </nav>
-
-      <main>Main</main>
-
-      <footer>Footer</footer>
+      <h1>Home page</h1>
     </div>
   );
 }
