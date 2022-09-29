53072adfc44007bd9c2078004c6891ac90799026
Yoyo Yang <yoyo.truly@gmail.com>
2022-09-29 14:49:13 -0400

    Add modified Nav component

---
+++ src/components/Layout/Nav.tsx
@@ -0,0 +1,41 @@
+import { useTheme as useNextTheme } from "next-themes";
+import { Navbar, Link, Switch, useTheme } from "@nextui-org/react";
+import NextLink from "next/link";
+
+export default function Nav() {
+  const { setTheme } = useNextTheme();
+  const { isDark, type } = useTheme();
+
+  return (
+    <Navbar isBordered variant="sticky" maxWidth="fluid">
+      <Navbar.Brand>
+        <NextLink href="/">
+          <Link>Buzzinga</Link>
+        </NextLink>
+      </Navbar.Brand>
+      <Navbar.Content activeColor="primary" variant="underline">
+        <NextLink href="/products">
+          <Navbar.Link isActive href="/products">
+            Products
+          </Navbar.Link>
+        </NextLink>
+        <NextLink href="/about">
+          <Navbar.Link>About</Navbar.Link>
+        </NextLink>
+        <NextLink href="/contacts">
+          <Navbar.Link href="/contacts">Contacts</Navbar.Link>
+        </NextLink>
+      </Navbar.Content>
+      <Navbar.Content>
+        <Navbar.Item>Sign Up</Navbar.Item>
+        <Navbar.Item>Log In</Navbar.Item>
+        <Navbar.Item>
+          <Switch
+            checked={isDark}
+            onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
+          />
+        </Navbar.Item>
+      </Navbar.Content>
+    </Navbar>
+  );
+}
