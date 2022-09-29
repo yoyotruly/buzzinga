2086ba7e9b608dcfdcccc6f5eb95e4d0a61cf2ac
Yoyo Yang <yoyo.truly@gmail.com>
2022-09-28 22:01:19 -0400

    Add styling to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,23 @@
+import { Box } from "./Box";
+
 export default function Footer() {
   return (
     <footer>
-      <p>Made with ❤️ by Yoyo Yang</p>
+      <Box
+        css={{
+          width: "100%",
+          height: "48px",
+          position: "fixed",
+          bottom: "0",
+          borderTop: "1px solid #eaeaea",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          fontSize: "0.8rem",
+        }}
+      >
+        Made with ❤️ by Yoyo Yang
+      </Box>
     </footer>
   );
 }
