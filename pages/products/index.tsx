76125152b6d9ff74fad67a6d4f0f438336cf3b92
Yoyo Yang <yoyo.truly@gmail.com>
2022-09-28 22:57:25 -0400

    Add product card placeholders

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,16 +1,95 @@
+import { Button, Card, Grid, Spacer, Text } from "@nextui-org/react";
 import Link from "next/link";
+import { Box } from "../../src/components/Box";
+
+const createFilterElements = (filters) => {
+  return (
+    <Grid.Container gap={1}>
+      {filters.map((filter) => (
+        <Grid key={filter}>
+          <Button size="sm" bordered color="primary" rounded auto ghost>
+            {filter}
+          </Button>
+        </Grid>
+      ))}
+    </Grid.Container>
+  );
+};
+
+const createProductCards = (products) => {
+  return (
+    <Grid.Container gap={3}>
+      {products.map((product) => (
+        <Grid key={product}>
+          <Card
+            isPressable
+            isHoverable
+            variant="flat"
+            css={{ width: "250px", height: "350px" }}
+          >
+            <Card.Body>
+              <Text>{product}</Text>
+            </Card.Body>
+          </Card>
+        </Grid>
+      ))}
+    </Grid.Container>
+  );
+};
 
 export default function Products() {
+  const categoryElements = createFilterElements([
+    "Beer",
+    "Cider",
+    "Wine",
+    "Spirits",
+  ]);
+
+  const subcategoryElements = createFilterElements([
+    "Lager",
+    "Ale",
+    "Stout",
+    "IPA",
+    "Cider",
+    "Red Wine",
+    "White Wine",
+    "Rose Wine",
+    "Vodka",
+    "Gin",
+    "Rum",
+    "Whiskey",
+  ]);
+
+  const productElements = createProductCards([
+    "Product 1",
+    "Product 2",
+    "Product 3",
+    "Product 4",
+    "Product 5",
+    "Product 6",
+    "Product 7",
+    "Product 8",
+    "Product 9",
+    "Product 10",
+  ]);
+
   return (
-    <>
-      <nav>
+    <Box
+      css={{
+        display: "flex",
+        width: "90%",
+        margin: "auto",
+        justifyContent: "space-between",
+      }}
+    >
+      <Box css={{ maxW: "350px" }}>
         <section>
           <h2>Category</h2>
-          <div>Pills</div>
+          {categoryElements}
         </section>
         <section>
           <h2>Sub-Category</h2>
-          <div>Pills</div>
+          {subcategoryElements}
         </section>
         <section>
           <h2>Price Range</h2>
@@ -20,10 +99,13 @@ export default function Products() {
           <h2>Sugar Content</h2>
           <div>Slider</div>
         </section>
-      </nav>
+      </Box>
 
-      <main>
+      <Spacer x={2} />
+
+      <Box css={{ flexGrow: 1 }}>
         <h1>Products List</h1>
+        {productElements}
         <div>
           <Link href="/products/[pid]" as={"/products/1"}>
             Product card 1
@@ -34,7 +116,7 @@ export default function Products() {
             Product card 2
           </Link>
         </div>
-      </main>
-    </>
+      </Box>
+    </Box>
   );
 }
