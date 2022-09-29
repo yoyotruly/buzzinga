fd5a7298cbb6f61f449e6156be7b1c5e477787dd
Yoyo Yang <yoyo.truly@gmail.com>
2022-09-29 18:53:28 -0400

    Replace styled layout component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,16 +1,23 @@
 import Nav from "./Nav";
 import Footer from "./Footer";
 import { Container } from "@nextui-org/react";
-import { StyledLayout } from "./Layout.styled";
 
 export default function Layout({ children }) {
   return (
-    <StyledLayout>
+    <Container
+      fluid
+      display="flex"
+      direction="column"
+      gap={0}
+      css={{ minHeight: "100vh" }}
+    >
       <Nav />
-      <Container xl css={{ flexGrow: 1 }}>
+
+      <Container as="main" fluid display="flex" css={{ flexGrow: 1 }}>
         {children}
       </Container>
+
       <Footer />
-    </StyledLayout>
+    </Container>
   );
 }
