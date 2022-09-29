a7ceff4b7c89090df0c645534ff97210d36406c2
Yoyo Yang <yoyo.truly@gmail.com>
2022-09-28 20:44:41 -0400

    Add theme toggle to Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,15 +1,34 @@
 import Link from "next/link";
+import { useTheme as useNextTheme } from "next-themes";
+import { Switch, useTheme } from "@nextui-org/react";
 
 export default function Nav() {
+  const { setTheme } = useNextTheme();
+  const { isDark, type } = useTheme();
+
   return (
     <nav>
       <ul>
-        <li><Link href="/">Buzzinga</Link></li>
-        <li><Link href="/products">Products</Link></li>
-        <li><Link href="/about">About Me</Link></li>
-        <li><Link href="/contacts">Contacts</Link></li>
+        <li>
+          <Link href="/">Buzzinga</Link>
+        </li>
+        <li>
+          <Link href="/products">Products</Link>
+        </li>
+        <li>
+          <Link href="/about">About Me</Link>
+        </li>
+        <li>
+          <Link href="/contacts">Contacts</Link>
+        </li>
         <li>Sign Up</li>
         <li>Log In</li>
+        <li>
+          <Switch
+            checked={isDark}
+            onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
+          />
+        </li>
       </ul>
     </nav>
   );
