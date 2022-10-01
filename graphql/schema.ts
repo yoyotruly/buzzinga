d90b57d2c63718ad560351df90f58dfbdf0072d9
Yoyo Yang <yoyo.truly@gmail.com>
2022-10-01 01:39:01 -0400

    Change manual schema declaration to using nexus

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,15 +1,20 @@
-import { gql } from "apollo-server-micro";
+import { makeSchema } from "nexus";
+import { join } from "path";
 
-export const typeDefs = gql`
-  type Product {
-    id: ID!
-    name: String!
-    price: Float
-    description: String
-    image: String
-  }
-
-  type Query {
-    products: [Product!]!
-  }
-`;
+export const schema = makeSchema({
+  types: [],
+  outputs: {
+    typegen: join(
+      process.cwd(),
+      "node-modules",
+      "@types",
+      "nexus-typegen",
+      "index.d.ts"
+    ),
+    schema: join(process.cwd(), "graphql", "schema.graphql"),
+  },
+  contextType: {
+    export: "Context",
+    module: join(process.cwd(), "graphql", "context.ts"),
+  },
+});
