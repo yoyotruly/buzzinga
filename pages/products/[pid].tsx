5fb4adf619be051bd5fd9acee3f63c65967099a1
Yoyo Yang <yoyo.truly@gmail.com>
2022-10-01 02:22:56 -0400

    Remove styled box from product detail page

--- pages/products/[pid].tsx
+++ pages/products/[pid].tsx
@@ -1,23 +1,22 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { Box } from "../../src/components/Box";
 
 export default function Product({ product }) {
   const router = useRouter();
   const { pid } = router.query;
 
   return (
-    <Box css={{ width: "90%", margin: "auto", backgroundColor: "yellow" }}>
-      <Box css={{ height: "600px" }}>
+    <div>
+      <div>
         <h1>Product {pid} Name</h1>
         <div>Description and stats</div>
         <div>Product image</div>
         <div>Product side stats</div>
-      </Box>
+      </div>
 
       <section>
         <h2>You may also like</h2>
-        <Box css={{ display: "flex" }}>
+        <div>
           <div>
             <Link href="/products/[pid]" as={"/products/1"}>
               Product card 1
@@ -28,8 +27,8 @@ export default function Product({ product }) {
               Product card 2
             </Link>
           </div>
-        </Box>
+        </div>
       </section>
-    </Box>
+    </div>
   );
 }
