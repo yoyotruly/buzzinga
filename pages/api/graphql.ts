b015a9ae3f34cba18f8a517ea661a2ba9b5608fa
Yoyo Yang <yoyo.truly@gmail.com>
2022-10-01 01:39:43 -0400

    Add cors and nexus schema to apollo server

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,10 +1,15 @@
+import Cors from "micro-cors";
 import { ApolloServer } from "apollo-server-micro";
 import { resolvers } from "../../graphql/resolvers";
-import { typeDefs } from "../../graphql/schema";
-import Cors from "micro-cors";
+import { schema } from "../../graphql/schema";
+import { createContext } from "../../graphql/context";
 
 const cors = Cors();
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
+const apolloServer = new ApolloServer({
+  schema,
+  resolvers,
+  context: createContext,
+});
 const startServer = apolloServer.start();
 
 export default cors(async function handler(req, res) {
