69bb71664df3958b7c9ed6ced1bf2a0da0386ba4
Yoyo Yang <yoyo.truly@gmail.com>
2022-10-01 20:51:21 -0400

    Modify hero UI on home page

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -1,25 +1,33 @@
-import { Button, Container, Text } from "@nextui-org/react";
+import { Button, Container, Link, Spacer, Text } from "@nextui-org/react";
+import NextLink from "next/link";
 
 export default function HomeHero() {
   return (
     <Container gap={0} css={{ margin: "auto" }}>
       <Text
         h1
-        size={80}
-        css={{ textGradient: "45deg, $purple600 -20%, $yellow600 100%" }}
-        weight="bold"
+        size="$8xl"
+        css={{
+          textGradient: "45deg, $purple600 30%, $yellow600 100%",
+          lineHeight: "100%",
+          paddingBottom: "0.05em",
+        }}
       >
         Just relax, and go with the flow
       </Text>
-      <p>
+      <Spacer />
+      <Text weight="light">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque
         cumque, ex expedita amet, veniam quibusdam nihil molestias corporis
         quaerat ducimus excepturi, veritatis labore iste perspiciatis eius omnis
         suscipit voluptatum magni.
-      </p>
-      <Button rounded color="secondary">
+      </Text>
+      <Spacer />
+      <NextLink href="/products">
+        {/* <Button rounded shadow color="secondary" animated size="lg"> */}
         Go Explore
-      </Button>
+        {/* </Button> */}
+      </NextLink>
     </Container>
   );
 }
