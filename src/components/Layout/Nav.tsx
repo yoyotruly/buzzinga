e15575cc8b0db7c0ef6152991f28b0cbbbab4773
Yoyo Yang <yoyo.truly@gmail.com>
2022-10-01 20:50:14 -0400

    Add outer Box to navbar

--- src/components/Layout/Nav.tsx
+++ src/components/Layout/Nav.tsx
@@ -1,45 +1,54 @@
 import { useTheme as useNextTheme } from "next-themes";
 import { Navbar, Link, Switch, useTheme } from "@nextui-org/react";
 import NextLink from "next/link";
+import Box from "../Utilities/Box.styled";
+import { border, activeColor } from "../Utilities/constants";
 
 export default function Nav() {
   const { setTheme } = useNextTheme();
   const { isDark, type } = useTheme();
 
   return (
-    <Navbar
-      variant="sticky"
-      maxWidth="fluid"
-      css={{ borderBottom: "$borderWeights$light solid $colors$border" }}
+    <Box
+      css={{
+        borderBottom: border,
+        position: "sticky",
+        top: 0,
+        zIndex: "$max",
+      }}
     >
-      <Navbar.Brand>
-        <NextLink href="/">
-          <Link color="secondary">Buzzinga</Link>
-        </NextLink>
-      </Navbar.Brand>
-      <Navbar.Content activeColor="secondary" variant="underline">
-        <NextLink href="/products">
-          <Navbar.Link isActive href="/products">
-            Products
-          </Navbar.Link>
-        </NextLink>
-        <NextLink href="/about">
-          <Navbar.Link>About</Navbar.Link>
-        </NextLink>
-        <NextLink href="/contacts">
-          <Navbar.Link href="/contacts">Contacts</Navbar.Link>
-        </NextLink>
-      </Navbar.Content>
-      <Navbar.Content>
-        <Navbar.Item>Sign Up</Navbar.Item>
-        <Navbar.Item>Log In</Navbar.Item>
-        <Navbar.Item>
-          <Switch
-            checked={isDark}
-            onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
-          />
-        </Navbar.Item>
-      </Navbar.Content>
-    </Navbar>
+      <Navbar maxWidth="xl">
+        <Navbar.Brand>
+          <NextLink href="/">
+            <Link color={activeColor}>Buzzinga</Link>
+          </NextLink>
+        </Navbar.Brand>
+        <Navbar.Content activeColor={activeColor} variant="underline">
+          <NextLink href="/products">
+            <Navbar.Link isActive href="/products">
+              Products
+            </Navbar.Link>
+          </NextLink>
+          <NextLink href="/about">
+            <Navbar.Link>About</Navbar.Link>
+          </NextLink>
+          <NextLink href="/contacts">
+            <Navbar.Link href="/contacts">Contacts</Navbar.Link>
+          </NextLink>
+        </Navbar.Content>
+        <Navbar.Content>
+          <Navbar.Item>Sign Up</Navbar.Item>
+          <Navbar.Item>Log In</Navbar.Item>
+          <Navbar.Item>
+            <Switch
+              shadow
+              color={activeColor}
+              checked={isDark}
+              onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
+            />
+          </Navbar.Item>
+        </Navbar.Content>
+      </Navbar>
+    </Box>
   );
 }
