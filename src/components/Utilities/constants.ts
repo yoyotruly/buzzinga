f3eb0ca08a38ab3723fbcdbec92f09d2e244ad4d
Yoyo Yang <yoyo.truly@gmail.com>
2022-10-01 20:45:30 -0400

    Refactor UI constants to its own file

---
+++ src/components/Utilities/constants.ts
@@ -0,0 +1,16 @@
+const footerHeight = "50px";
+const border = "$borderWeights$light solid $colors$border";
+const productFilterWidth = "350px";
+const productCardWidth = "245px";
+const productCardHeight = "350px";
+const productGridGap = 1.7;
+const activeColor = "secondary";
+
+export {
+  footerHeight,
+  border,
+  activeColor,
+  productCardHeight,
+  productCardWidth,
+  productGridGap,
+};
