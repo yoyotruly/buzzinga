dda9b62c4d6b62c09c5f2b4156faebb7da25401e
Yoyo Yang <yoyo.truly@gmail.com>
2022-10-01 20:49:57 -0400

    Replace layout outer container with Box component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,23 +1,16 @@
 import Nav from "./Nav";
 import Footer from "./Footer";
 import { Container } from "@nextui-org/react";
+import Box from "../Utilities/Box.styled";
 
 export default function Layout({ children }) {
   return (
-    <Container
-      fluid
-      display="flex"
-      direction="column"
-      gap={0}
-      css={{ minHeight: "100vh" }}
-    >
+    <Box css={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
       <Nav />
-
-      <Container as="main" fluid display="flex" css={{ flexGrow: 1 }}>
+      <Container as="main" xl display="flex" css={{ flexGrow: 1 }}>
         {children}
       </Container>
-
       <Footer />
-    </Container>
+    </Box>
   );
 }
