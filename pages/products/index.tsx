09f012231b352b8b5cd09f5c008572b2b891bab5
Yoyo Yang <yoyo.truly@gmail.com>
2022-10-02 23:01:58 -0400

    Add pagination to products page

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -5,32 +5,41 @@ import ProductCard from "../../src/components/Product/ProductCard";
 import Link from "next/link";
 import { productGridGap } from "../../src/utilities/constants";
 
-const ProductsQuery = gql`
-  query {
-    products {
-      id
-      name
-      image
+const GetProducts = gql`
+  query GetProductsQuery($first: Int, $after: Int) {
+    products(first: $first, after: $after) {
+      edges {
+        cursor
+        node {
+          id
+          name
+          image
+        }
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
     }
   }
 `;
 
 export default function Products() {
-  const { loading, error, data } = useQuery(ProductsQuery);
+  const { loading, error, data, fetchMore } = useQuery(GetProducts, {
+    variables: { first: 2 },
+  });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Oops, something went wrong {error.message}</p>;
 
+  const { endCursor, hasNextPage } = data.products.pageInfo;
+
   const productElements = (
     <Grid.Container gap={productGridGap}>
-      {data?.products.map((product) => (
-        <Link
-          key={product.id}
-          href="/products/[pid]"
-          as={`/products/${product.id}`}
-        >
+      {data?.products.edges.map(({ node }) => (
+        <Link key={node.id} href="/products/[pid]" as={`/products/${node.id}`}>
           <Grid>
-            <ProductCard product={product} />
+            <ProductCard product={node} />
           </Grid>
         </Link>
       ))}
@@ -38,14 +47,36 @@ export default function Products() {
   );
 
   return (
-    <Grid.Container wrap="nowrap" css={{ padding: "$13 0" }}>
-      <Grid as="section" xs={2.5}>
-        <FilterGroup />
-      </Grid>
+    <>
+      <Grid.Container wrap="nowrap" css={{ padding: "$13 0" }}>
+        <Grid as="section" xs={2.5}>
+          <FilterGroup />
+        </Grid>
 
-      <Grid as="section" xs>
-        <Container css={{ paddingRight: 0 }}>{productElements}</Container>
-      </Grid>
-    </Grid.Container>
+        <Grid as="section" xs>
+          <Container css={{ paddingRight: 0 }}>{productElements}</Container>
+        </Grid>
+      </Grid.Container>
+      {hasNextPage ? (
+        <button
+          onClick={() => {
+            fetchMore({
+              variables: { after: parseInt(endCursor) },
+              updateQuery: (prev, { fetchMoreResult }) => {
+                fetchMoreResult.products.edges = [
+                  ...prev.products.edges,
+                  ...fetchMoreResult.products.edges,
+                ];
+                return fetchMoreResult;
+              },
+            });
+          }}
+        >
+          Load More
+        </button>
+      ) : (
+        <p> No more products </p>
+      )}
+    </>
   );
 }
