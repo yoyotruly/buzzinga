1a4f0c95469dbdf9c6af4abbfb2ef48890346791
Yoyo Yang <yoyo.truly@gmail.com>
2022-10-02 20:36:27 -0400

    Add timestamp fields to user graphql type

--- graphql/types/User.ts
+++ graphql/types/User.ts
@@ -4,9 +4,11 @@ import { Product } from "./Product";
 export const User = objectType({
   name: "User",
   definition(t) {
-    t.string("id");
+    t.int("id");
     t.string("email");
     t.string("name");
+    t.string("createdAt");
+    t.string("updatedAt");
     t.list.field("favorites", {
       type: Product,
       async resolve(parent, _args, ctx) {
